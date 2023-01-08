fb56f05a605e123cfc6589f955aa8eee498cbc23
sn <shurick2211@gmail.com>
2023-01-08 18:12:39 +0200

    i18 - ?

--- src/main/java/com/nimko/shppmentorpracktic7/controllers/MyAdviceController.java
+++ src/main/java/com/nimko/shppmentorpracktic7/controllers/MyAdviceController.java
@@ -1,6 +1,8 @@
 package com.nimko.shppmentorpracktic7.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+import java.util.Locale;
 import java.util.NoSuchElementException;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
@@ -17,6 +21,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @ControllerAdvice
 public class MyAdviceController {
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
@@ -32,8 +37,8 @@ public class MyAdviceController {
     @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
     @ResponseBody
     public ResponseEntity<?> notFoundError(NoSuchElementException exception) {
-        String mess = exception.getMessage() != null ? exception.getMessage() :
-                ResourceBundle.getBundle("messages").getString("operation.null");
+
+        String mess = exception.getMessage() != null ? exception.getMessage() :"Operation is wrong!";
         log.error(mess);
         return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(mess);
     }
